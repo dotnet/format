8cab96141dca4f351e5cb2a798db6288229a155e
Joey Robichaud <jorobich@microsoft.com>
2019-04-08 10:52:03 -0700

    Update pool

--- eng/format-verifier.ps1
+++ eng/format-verifier.ps1
@@ -10,11 +10,11 @@ $restoreLog = Join-Path $logPath "restore.log"
 $formatLog = Join-Path $logPath "format.log"
 
 If (!(Test-Path $testPath)) {
-    New-Item -ItemType Directory -Force -Path $testPath
+    New-Item -ItemType Directory -Force -Path $testPath | Out-Null
 }
 
 If (!(Test-Path $logPath)) {
-    New-Item -ItemType Directory -Force -Path $logPath
+    New-Item -ItemType Directory -Force -Path $logPath | Out-Null
 }
 
 try {
@@ -23,20 +23,22 @@ try {
 
     $repoPath = Join-Path $testPath $folderName
 
-    Write-Output "$(Get-Date) - Cloning $repoName..."
+    Write-Output "$(Get-Date) - Cloning $repoName."
     git.exe clone --depth 1 $repo $repoPath *> $cloneLog
 
-    Write-Output "$(Get-Date) - Finding solutions..."
+    Write-Output "$(Get-Date) - Finding solutions."
     $solutions = Get-ChildItem -Path $repoPath -Filter *.sln -Recurse -Depth 2 | Select-Object -ExpandProperty FullName
 
     foreach ($solution in $solutions) {
-        Write-Output "$(Get-Date) - Restoring $solution..."
+        $solutionFile = Split-Path $solution -leaf
+
+        Write-Output "$(Get-Date) - Restoring  $solutionFile."
         dotnet.exe restore $solution *> $restoreLog
 
-        Write-Output "$(Get-Date) - Formatting $solution..."
+        Write-Output "$(Get-Date) - Formatting $solutionFile."
         dotnet.exe run -p .\src\dotnet-format.csproj -c Release -- -w $solution -v d --dry-run *> $formatLog
         
-        if ($LastExitCode -ne 0) { 
+        if ($LastExitCode -ne 0) {
             Write-Output "$(Get-Date) - Formatting failed with error code $LastExitCode. Check the format.log for details."
             exit -1
         }
@@ -47,7 +49,7 @@ try {
             exit -1
         }
 
-        Write-Output "$(Get-Date) - Formatted $solution."
+        Write-Output "$(Get-Date) - Formatted  $solutionFile."
     }
 }
 catch {
@@ -56,8 +58,6 @@ catch {
 finally {
     Remove-Item $repoPath -Force -Recurse
     Write-Output "$(Get-Date) - Deleted $repoName."
-
-    Write-Output ""       
 }
 
 Remove-Item *.log
