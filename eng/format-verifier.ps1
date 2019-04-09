0244f9306a32cb1a8bd46b70f905e71dfac632cd
Joey Robichaud <jorobich@microsoft.com>
2019-04-08 17:02:36 -0700

    Check specific repo shas

--- eng/format-verifier.ps1
+++ eng/format-verifier.ps1
@@ -1,24 +1,36 @@
 [CmdletBinding(PositionalBinding = $false)]
 Param(
     [string]$repo,
+    [string]$sha,
     [string]$testPath
 )
 
-If (!(Test-Path $testPath)) {
+function Clone-Repo([string]$repo, [string]$sha, [string]$repoPath) {
+    $currentLocation = Get-Location
+
+    git.exe clone $repo $repoPath
+
+    Set-Location $repoPath
+
+    git.exe checkout $sha
+
+    Set-Location $currentLocation
+}
+
+if (!(Test-Path $testPath)) {
     New-Item -ItemType Directory -Force -Path $testPath | Out-Null
 }
 
 try {
     $repoName = $repo.Substring(19)
     $folderName = $repoName.Split("/")[1]
-
     $repoPath = Join-Path $testPath $folderName
 
     Write-Output "$(Get-Date) - Cloning $repoName."
-    git.exe clone --depth 1 $repo $repoPath
+    Clone-Repo $repo, $sha, $repoPath
 
     Write-Output "$(Get-Date) - Finding solutions."
-    $solutions = Get-ChildItem -Path $repoPath -Filter *.sln -Recurse -Depth 2 | Select-Object -ExpandProperty FullName | Where-Object {$_ -match '.sln$'}
+    $solutions = Get-ChildItem -Path $repoPath -Filter *.sln -Recurse -Depth 2 | Select-Object -ExpandProperty FullName | Where-Object { $_ -match '.sln$' }
 
     foreach ($solution in $solutions) {
         $solutionFile = Split-Path $solution -leaf
