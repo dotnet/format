a45571e21fa6e7c1212828ff3ba33b7858352dde
Jonathon Marolf <jmarolf@microsoft.com>
2019-07-23 08:51:45 -0700

    make implementation concurrent

--- src/Analyzers/ConcurrentAnalyzerRunner.cs
+++ src/Analyzers/ConcurrentAnalyzerRunner.cs
@@ -2,7 +2,6 @@
 
 #nullable enable
 
-using System;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
@@ -21,38 +20,32 @@ namespace Microsoft.CodeAnalysis.Tools.Analyzers
 
         public static IAnalyzerRunner Instance { get; } = new ConcurrentAnalyzerRunner();
 
-        public async Task<CodeAnalysisResult> RunCodeAnalysisAsync(ImmutableArray<DiagnosticAnalyzer> analyzers,
-                                                                   ImmutableArray<(Document Document, OptionSet OptionSet, ICodingConventionsSnapshot CodingConventions)> formattableDocuments,
-                                                                   ILogger logger,
-                                                                   CancellationToken cancellationToken)
+        public Task<CodeAnalysisResult> RunCodeAnalysisAsync(ImmutableArray<DiagnosticAnalyzer> analyzers,
+                                                             ImmutableArray<(Document Document, OptionSet OptionSet, ICodingConventionsSnapshot CodingConventions)> formattableDocuments,
+                                                             ILogger logger,
+                                                             CancellationToken cancellationToken)
         {
-            var solution = formattableDocuments.FirstOrDefault().Document?.Project.Solution;
-            if (solution is null)
-            {
-                logger.LogError(NoFormattableDocuments);
-                throw new InvalidOperationException(NoFormattableDocuments);
-            }
-
-            var documents = formattableDocuments.Select(x => x.Document).ToList();
             var result = new CodeAnalysisResult();
-            foreach (var project in solution.Projects)
+            var solution = formattableDocuments.FirstOrDefault().Document.Project.Solution;
+            var documents = formattableDocuments.Select(x => x.Document).ToList();
+            Parallel.ForEach(solution.Projects, project =>
             {
-                var compilation = await project.GetCompilationAsync(cancellationToken);
+                var compilation = project.GetCompilationAsync(cancellationToken).GetAwaiter().GetResult();
                 // TODO: generate option set to ensure the analyzers run
                 // TODO: Ensure that the coding conventions snapshop gets passed to the analyzers somehow
                 var analyzerCompilation = compilation.WithAnalyzers(analyzers, options: null, cancellationToken);
-                var diagnosticResult = await analyzerCompilation.GetAllDiagnosticsAsync(cancellationToken);
+                var diagnosticResult = analyzerCompilation.GetAllDiagnosticsAsync(cancellationToken).GetAwaiter().GetResult();
                 foreach (var diagnostic in diagnosticResult)
                 {
                     var doc = documents.Find(d => d.FilePath == diagnostic.Location.GetLineSpan().Path);
                     if (doc != null)
                     {
-                        result.AddDiagnostic(doc, x);
+                        result.AddDiagnostic(doc, diagnostic);
                     }
                 }
-            }
+            });
 
-            return result;
+            return Task.FromResult(result);
         }
     }
 }
