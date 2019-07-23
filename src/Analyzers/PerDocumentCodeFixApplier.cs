09580bcde0ecc91ed4b165d8f5ef6af159eb9631
Rikki Gibson <rigibson@microsoft.com>
2019-07-23 15:23:21 -0700

    Fix all diagnostics for code fix in solution

--- src/Analyzers/PerDocumentCodeFixApplier.cs
+++ src/Analyzers/PerDocumentCodeFixApplier.cs
@@ -1,23 +1,77 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-
+using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.CodeAnalysis.Tools.Analyzers
 {
-    internal class PerDocumentCodeFixApplier : ICodeFixApplier
+    internal sealed class DiagnosticProvider : FixAllContext.DiagnosticProvider
     {
-        public Task<Solution> ApplyCodeFixesAsync(Solution solution,
-                                                  CodeAnalysisResult result,
-                                                  CodeFixProvider codefixes,
-                                                  ILogger logger,
-                                                  CancellationToken cancellationToken)
+        private readonly IReadOnlyDictionary<Project, ImmutableArray<Diagnostic>> _diagnosticsByProject;
+
+        internal DiagnosticProvider(CodeAnalysisResult analysisResult)
+        {
+            _diagnosticsByProject = analysisResult.Diagnostics;
+        }
+
+        public override Task<IEnumerable<Diagnostic>> GetAllDiagnosticsAsync(Project project, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<IEnumerable<Diagnostic>>(_diagnosticsByProject[project]);
+        }
+
+        public override Task<IEnumerable<Diagnostic>> GetDocumentDiagnosticsAsync(Document document, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
+        }
+
+        public override Task<IEnumerable<Diagnostic>> GetProjectDiagnosticsAsync(Project project, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<IEnumerable<Diagnostic>>(_diagnosticsByProject[project]);
+        }
+    }
+
+    internal class PerDocumentCodeFixApplier : ICodeFixApplier
+    {
+        public async Task<Solution> ApplyCodeFixesAsync(
+            Solution solution,
+            CodeAnalysisResult result,
+            CodeFixProvider codeFix,
+            ILogger logger,
+            CancellationToken cancellationToken)
+        {
+            var fixAllProvider = codeFix.GetFixAllProvider();
+            if (!fixAllProvider.GetSupportedFixAllScopes().Contains(FixAllScope.Solution))
+            {
+                throw new InvalidOperationException($"Code fix {codeFix.GetType()} doesn't support Fix All in Solution");
+            }
+
+            var project = solution.Projects.FirstOrDefault();
+            if (project == null)
+            {
+                throw new InvalidOperationException($"Solution {solution} has no projects");
+            }
+
+            var fixAllContext = new FixAllContext(
+                project: project,
+                codeFixProvider: codeFix,
+                scope: FixAllScope.Solution,
+                codeActionEquivalenceKey: null,
+                diagnosticIds: codeFix.FixableDiagnosticIds,
+                fixAllDiagnosticProvider: new DiagnosticProvider(result),
+                cancellationToken: cancellationToken);
+
+            var action = await fixAllProvider.GetFixAsync(fixAllContext);
+            var operations = await action.GetOperationsAsync(cancellationToken);
+            var applyChangesOperation = operations.OfType<ApplyChangesOperation>().SingleOrDefault();
+            // TODO: if there's nothing to fix, is the ApplyChangesOperation null?
+            return applyChangesOperation.ChangedSolution;
         }
     }
 }
