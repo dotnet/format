fda64f797150c87d732311f79d0d269971c93ab0
Joey Robichaud <jorobich@microsoft.com>
2019-07-23 17:44:43 -0700

    Filter surpressed diagnostics and suggestions

--- src/Analyzers/ConcurrentAnalyzerRunner.cs
+++ src/Analyzers/ConcurrentAnalyzerRunner.cs
@@ -32,7 +32,9 @@ namespace Microsoft.CodeAnalysis.Tools.Analyzers
             var diagnostics = await analyzerCompilation.GetAllDiagnosticsAsync(cancellationToken);
             // filter diagnostics
             var filteredDiagnostics = diagnostics.Where(
-                x => x.Location.IsInSource &&
+                x => !x.IsSuppressed &&
+                     x.Severity >= DiagnosticSeverity.Warning &&
+                     x.Location.IsInSource &&
                      formattableDocumentPaths.Contains(x.Location.SourceTree.FilePath, StringComparer.OrdinalIgnoreCase));
             result.AddDiagnostic(project, filteredDiagnostics);
         }
