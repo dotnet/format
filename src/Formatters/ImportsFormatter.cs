889bf045874231089e35633e9a65c063bec080e5
Joey Robichaud <jorobich@microsoft.com>
2019-07-25 09:51:07 -0700

    Add import formatter to sort and group imports

---
+++ src/Formatters/ImportsFormatter.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.ExternalAccess.Format;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.CodingConventions;
+
+namespace Microsoft.CodeAnalysis.Tools.Formatters
+{
+    /// <summary>
+    /// CodeFormatter that uses the <see cref="ImportsOrganizer"/> to format document imports.
+    /// </summary>
+    internal sealed class ImportsFormatter : DocumentFormatter
+    {
+        public override FormatType FormatType => FormatType.CodeStyle;
+        protected override string FormatWarningDescription => Resources.Fix_whitespace_formatting;
+
+        protected override async Task<SourceText> FormatFileAsync(
+            Document document,
+            SourceText sourceText,
+            OptionSet options,
+            ICodingConventionsSnapshot codingConventions,
+            FormatOptions formatOptions,
+            ILogger logger,
+            CancellationToken cancellationToken)
+        {
+            var organizedDocument = await ImportsOrganizer.OrganizeImportsAsync(document, cancellationToken);
+            if (organizedDocument == document)
+            {
+                return sourceText;
+            }
+
+            return await organizedDocument.GetTextAsync(cancellationToken);
+        }
+    }
+}
