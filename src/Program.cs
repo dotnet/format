24a214f3f35652d26ab953853854419cfbdebddd
Adeel <adeelbm@outlook.com>
2020-02-16 02:46:26 +0000

    Use space-separated paths instead of comma-separated

--- src/Program.cs
+++ src/Program.cs
@@ -34,8 +34,8 @@ namespace Microsoft.CodeAnalysis.Tools
                 .UseExceptionHandler()
                 .AddOption(new Option(new[] { "--folder", "-f" }, Resources.The_folder_to_operate_on_Cannot_be_used_with_the_workspace_option, new Argument<string>(() => null)))
                 .AddOption(new Option(new[] { "--workspace", "-w" }, Resources.The_solution_or_project_file_to_operate_on_If_a_file_is_not_specified_the_command_will_search_the_current_directory_for_one, new Argument<string>(() => null)))
-                .AddOption(new Option(new[] { "--include", "--files" }, Resources.A_comma_separated_list_of_relative_file_or_folder_paths_to_include_in_formatting_All_files_are_formatted_if_empty, new Argument<string>(() => null)))
-                .AddOption(new Option(new[] { "--exclude" }, Resources.A_comma_separated_list_of_relative_file_or_folder_paths_to_exclude_from_formatting, new Argument<string>(() => null)))
+                .AddOption(new Option(new[] { "--include", "--files" }, Resources.A_list_of_relative_file_or_folder_paths_to_include_in_formatting_All_files_are_formatted_if_empty, new Argument<string[]>(() => null)))
+                .AddOption(new Option(new[] { "--exclude" }, Resources.A_list_of_relative_file_or_folder_paths_to_exclude_from_formatting, new Argument<string[]>(() => null)))
                 .AddOption(new Option(new[] { "--check", "--dry-run" }, Resources.Formats_files_without_saving_changes_to_disk_Terminate_with_a_non_zero_exit_code_if_any_files_were_formatted, new Argument<bool>()))
                 .AddOption(new Option(new[] { "--report" }, Resources.Accepts_a_file_path_which_if_provided_will_produce_a_format_report_json_file_in_the_given_directory, new Argument<string>(() => null)))
                 .AddOption(new Option(new[] { "--verbosity", "-v" }, Resources.Set_the_verbosity_level_Allowed_values_are_quiet_minimal_normal_detailed_and_diagnostic, new Argument<string>() { Arity = ArgumentArity.ExactlyOne }.FromAmong(_verbosityLevels)))
@@ -45,7 +45,7 @@ namespace Microsoft.CodeAnalysis.Tools
             return await parser.InvokeAsync(args).ConfigureAwait(false);
         }
 
-        public static async Task<int> Run(string folder, string workspace, string verbosity, bool check, string include, string exclude, string report, IConsole console = null)
+        public static async Task<int> Run(string folder, string workspace, string verbosity, bool check, string[] include, string[] exclude, string report, IConsole console = null)
         {
             // Setup logging.
             var serviceCollection = new ServiceCollection();
@@ -194,24 +194,24 @@ namespace Microsoft.CodeAnalysis.Tools
         }
 
         /// <summary>
-        /// Converts a comma-separated list of relative file paths to a hashmap of full file paths.
+        /// Converts array of relative file paths to a hashmap of full file paths.
         /// </summary>
-        internal static ImmutableHashSet<string> GetRootedPaths(string paths, string folder)
+        internal static ImmutableHashSet<string> GetRootedPaths(string[] paths, string folder)
         {
-            if (string.IsNullOrEmpty(paths))
+            if (paths == null)
             {
-                return ImmutableHashSet.Create<string>();
+                return ImmutableHashSet<string>.Empty;
             }
 
             if (string.IsNullOrEmpty(folder))
             {
-                return paths.Split(',')
+                return paths
                     .Select(path => Path.GetFullPath(path, Environment.CurrentDirectory))
                     .ToImmutableHashSet(StringComparer.OrdinalIgnoreCase);
             }
             else
             {
-                return paths.Split(',')
+                return paths
                     .Select(path => Path.GetFullPath(path, Environment.CurrentDirectory))
                     .Where(path => path.StartsWith(folder))
                     .ToImmutableHashSet(StringComparer.OrdinalIgnoreCase);
